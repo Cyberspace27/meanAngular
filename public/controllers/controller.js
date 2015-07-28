310e63dde3dfec073dcfe75fc485e1e3ce17712c
Cyberspace27 <ariel.villarreal26@gmail.com>
2015-07-27 23:22:03 -0600

    Se finaliza la apliacaion con la funcionalidades de update y delete

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -27,5 +27,25 @@ myApp.controller('AppCtrl',['$scope', '$http',
 				refresh();
 			});
 		};	
+
+		$scope.edit = function(id){
+			console.log(id);
+			$http.get('/contactlist/' + id).success(function(response) {
+				$scope.contact = response;
+				
+			});
+		};
+
+		$scope.update = function(){
+			console.log($scope.contact._id);
+			$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
+				refresh();
+			});
+		};
+
+		$scope.deselect = function(){
+			$scope.contact = "";
+		};
+
 		
 	}]);
